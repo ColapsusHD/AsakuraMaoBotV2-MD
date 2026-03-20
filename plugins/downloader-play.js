61dc126c209ef414d5e16222af4f89f580bc0f86
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2026-03-20 20:41:35 -0300

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
       try {
         const ruby = await (
           await fetch(
-            `https://ruby-core.vercel.app/api/download/youtube/mp3?url=${encodeURIComponent(regex + result.videoId)}`
+            `https://api.siputzx.my.id/api/d/ytmp3?url=${encodeURIComponent(regex + result.videoId)}`
           )
         ).json();
         if (ruby?.status && ruby?.download?.url) {
